bae7ed2bd7dd583844b607c22c157e9aa5f07bd0
DeuryZ <119361476+DeuryZ@users.noreply.github.com>
2023-09-20 16:33:53 -0500

    feat: :lipstick:

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const
+function uno(){
+    var continent = 1014000000;
+    var country = 51609000;
+    var department = 2360000;
+    var city = 625114;
+    var neighborhood = "Alvarez";
+    var direction = "Calle 12 # 12 - 33";
+    console.log(continent + " " + country + " " + department + " " + city + " " + neighborhood + " " + direction);
+}
+function dos(){
+    
+}
